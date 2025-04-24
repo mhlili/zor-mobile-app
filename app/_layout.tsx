833b9ab7e1abbeee38c4d667987fcddde93d7b52
emilylin1 <94324447+emilylin1@users.noreply.github.com>
2025-04-24 17:24:02 -0400

    hide screen headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,21 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useColorScheme } from '@/components/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useColorScheme } from "@/components/useColorScheme";
 
-
-//AsyncStorage.removeItem('@completedOnboarding');
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,10 +25,10 @@ export default function RootLayout() {
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
-  
-  // Explicit type for useState, `boolean | null` ensures we handle the loading state properly.
-  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean | null>(null);
 
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<
+    boolean | null
+  >(null);
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -41,11 +38,13 @@ export default function RootLayout() {
   useEffect(() => {
     const checkOnboardingStatus = async () => {
       try {
-        const onboardingState = await AsyncStorage.getItem('@completedOnboarding');
-        console.log('Onboarding status:', onboardingState); // Debugging
-        setIsOnboardingCompleted(onboardingState === 'true'); // Ensure boolean conversion
+        const onboardingState = await AsyncStorage.getItem(
+          "@completedOnboarding",
+        );
+        console.log("Onboarding status:", onboardingState); // Debugging
+        setIsOnboardingCompleted(onboardingState === "true"); // Ensure boolean conversion
       } catch (error) {
-        console.error('Error fetching onboarding status:', error);
+        console.error("Error fetching onboarding status:", error);
         setIsOnboardingCompleted(false); // Default to false if an error occurs
       }
     };
@@ -67,29 +66,26 @@ export default function RootLayout() {
   return <RootLayoutNav isOnboardingCompleted={isOnboardingCompleted} />;
 }
 
-function RootLayoutNav({ isOnboardingCompleted }: { isOnboardingCompleted: boolean }) {
+function RootLayoutNav({
+  isOnboardingCompleted,
+}: {
+  isOnboardingCompleted: boolean;
+}) {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         {!isOnboardingCompleted ? (
-          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+          <Stack.Screen name="onboarding" />
         ) : (
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" />
         )}
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        <Stack.Screen name="fullCalendar" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="log-data-screens/logSeizure"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="log-data-screens/dailySurvey"
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        <Stack.Screen name="fullCalendar" />
+        <Stack.Screen name="log-data-screens/logSeizure" />
+        <Stack.Screen name="log-data-screens/dailySurvey" />
       </Stack>
     </ThemeProvider>
   );
 }
-
